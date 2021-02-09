5f30668760a26fb5ad55c83f996927c4947e8e8f
lorena-1204 <mlrc1204@gmail.com>
2021-02-09 10:04:23 -0500

    Primera maquetacion (html)

--- src/data.js
+++ src/data.js
@@ -1,6 +1,11 @@
+export const get =(personaje) => {
+  let container = document.querySelector (".container-characters");
+  container.innerHTML +=  <div class= "character"></div>
+}
+
 // estas funciones son de ejemplo
 
-export const example = () => {
+/*export const example = () => {
   return 'example';
 };
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
-import { example } from './data.js';
+import{get} from "./data.js";
+
+import date from "./data/pokemon/pokemon.js"
+
+data.results.dateforEach(get);
+
+
+/*import { example } from './data.js';
 // import data from './data/lol/lol.js';
 import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
