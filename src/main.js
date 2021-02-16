d4b13dc780989cbc22532f966a8e1daed27aa8e0
wendyhachilla <wendyhachilla2@gmail.com>
2021-02-16 11:50:50 -0500

    avance js

--- src/main.js
+++ src/main.js
@@ -1,27 +1,83 @@
 /**import{get} from "./data.js";*/
 // traer data, con forEach, con innerHTML para mostrar la data según le vamos indicando
-import datapoke from "./data/pokemon/pokemon.js"
-const contenedorData = document.getElementById("card");
-let traerData = datapoke.pokemon;
-let cardPokemon = traerData.forEach((dataPokemon) => {
-    return contenedorData.innerHTML +=
-    //  utilización de template strings (`)
-     `<figure>
-        <img width = "200" src = "${dataPokemon.img}" >
-     
-        <p>#${dataPokemon.num}</p>
-        <p>${dataPokemon.name}</p>
-        
-      <figcaption>
-        <p> CP max: ${dataPokemon.stats["max-cp"]} </p> 
-        <p> HP max: ${dataPokemon.stats["max-hp"]} </p>
-      </figcaption>
-     
-     </figure>`
 
+import datapoke from "./data/pokemon/pokemon.js";
+import { pokemonOrder} from "./data.js";
+
+
+const dataContainer = document.getElementById("card");
+
+//variable que llama a toda la data
+let everyPokemon = datapoke.pokemon;
+
+
+let cardPokemon = (dataPokemon) => {
+  
+  dataPokemon.forEach((pokemon) => {
+
+    const container = document.createElement('figure');
+    dataContainer.appendChild(container).innerHTML =
+     //  utilización de template strings (`)
+
+      `
+         <img class="img-poke"  src = "${pokemon.img}" >
+                 
+       <figcaption>
+         <p>#${pokemon.num}</p>
+         <p>${pokemon.name}</p>
+         <p> CP max: ${pokemon.stats["max-cp"]} </p> 
+         <p> HP max: ${pokemon.stats["max-hp"]} </p>
+       </figcaption>
+     
+      `
+
+  // dataContainer.appenChild(openCards);
+  // openCards.innerHTML = cardPokemon;
+     
+})
+};
+
+cardPokemon(everyPokemon);
+
+//función que permite ordenar según la opción a ordenar
+const differentOrder = document.getElementById("differentOrder");
+
+differentOrder.addEventListener("change", () => {
+  dataContainer.innerHTML = "";
+  const selected = differentOrder.value;
+  const dataOrder = pokemonOrder.differentOrder(everyPokemon, selected);
+  cardPokemon(dataOrder);
+  // console.log("DatAORder", cardPokemon(dataOrder))
+
+})
+
+
+
+
+//variable para llamar al selector de clase
+// const theCards = document.querySelector("the-Cards")
+
+//Función para mostrar a los pokemons
+// const showPokemons = (datapoke) => {
+//   let pokemonPack = "";
+
+//   for (const pokemon in data) {
+//     const lookCard = document.createElement("section");
+
+//     //template strings
+//     pokemonPack = `
+//     <section id="lookCard"> 
+//       <section class="frontCard">
+//           <p id=""    
     
-     
-});
+//     </section>
 
 
+
+
+
+//   }
+
+
+// }
 
