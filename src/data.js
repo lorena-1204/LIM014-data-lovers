c0c7c26f3633b8f0fadbcd91eb30d6e7d775ffe0
lorena-1204 <mlrc1204@gmail.com>
2021-02-18 10:24:53 -0500

    avance de diseño3

---
+++ src/data.js
@@ -0,0 +1,75 @@
+export const pokemonOrder = {
+    differentOrder: (everyPokemon, selected) => {
+        let result;
+
+        if (selected === "aZ"){
+            result = everyPokemon.sort(orderByName);
+        }
+        if (selected === "aZ"){
+            result = everyPokemon.sort(orderByName).reverse();
+        }
+        if (selected === "ascendingNumber"){
+            result = everyPokemon.sort(orderByNumber);                        
+        }
+        if (selected === "descendingNumber"){
+            result = everyPokemon.sort(orderByNumber).reverse();
+        }
+        if (selected === "ascendingCp"){
+            result = everyPokemon.sort(orderByPc);            
+        }
+        if (selected === "descendingCp"){
+            result = everyPokemon.sort(orderByPc).reverse();            
+        }
+        if (selected === "ascendingHp"){
+            result = everyPokemon.sort(orderByHp);
+        }
+        if (selected === "descendingHp"){
+            result = everyPokemon.sort(orderByHp).reverse();
+        }
+
+        //función para ordenar la data según el nombre
+        function orderByName(a,b) {
+            if (a.name < b.name) {
+                return -1;
+            }
+            if (a.name > b.name) {
+                return 1;
+            }
+            return 0;
+        }
+
+        //función para ordenar la data según el número
+        function orderByNumber(a,b) {
+            if (a.num < b.num) {
+                return -1;                
+            }
+            if (a.num > b.num) {
+                return 1;
+            }
+            return 0;
+        }
+        
+        //función para ordenar la data según PC
+        function orderByPc(a,b) {
+            if (parseInt(a.stats["max-cp"]) < parseInt(b.stats["max-cp"])) {
+                return -1;
+            }
+            if (parseInt(a.stats["max-cp"]) > parseInt(b.stats["max-cp"])) {
+                return 1;
+            }
+            return 0;
+        }
+        
+        //función para ordenar la data según HP
+        function orderByHp(a,b) {
+            if (parseInt(a.stats["max-hp"]) < parseInt(b.stats["max-hp"])) {
+                return -1;                
+            }
+            if (parseInt(a.stats["max-hp"]) > parseInt(b.stats["max-hp"])) {
+                return 1;
+            }
+            return 0;
+        }
+        return result;
+    }
+};
--- src/main.js
+++ src/main.js
@@ -1,24 +1,79 @@
 /**import{get} from "./data.js";*/
 // traer data, con forEach, con innerHTML para mostrar la data según le vamos indicando
-import datapoke from "./data/pokemon/pokemon.js"
-const contenedorData = document.getElementById("card");
-let traerData = datapoke.pokemon;
-let cardPokemon = traerData.forEach((dataPokemon) => {
-    return contenedorData.innerHTML +=
-    //  utilización de template strings (`)
-    
-     `
-      <figure class="card-poke">
 
-        <img src = "${dataPokemon.img}">
+import datapoke from "./data/pokemon/pokemon.js";
+import { pokemonOrder} from "./data.js";
+
+
+const dataContainer = document.getElementById("card");
+
+//variable que llama a toda la data
+let everyPokemon = datapoke.pokemon;
+
+let cardPokemon = (dataPokemon) => {
+  
+  dataPokemon.forEach((pokemon) => {
+
+    const container = document.createElement('article');
+    dataContainer.appendChild(container).innerHTML =
+     //  utilización de template strings (`)
+
+      `<figure class="container-border">
+        <div class="container-Card">
+          <img  src = "${pokemon.img}">
+         
+        
+       
+          <p> N° ${pokemon.num}</p>
+          <p>${pokemon.name}</p>
+          <p> CP max: ${pokemon.stats["max-cp"]} </p> 
+          <p> HP max: ${pokemon.stats["max-hp"]} </p>
+       
+        </div>   
+       </figure>`
+
+  // dataContainer.appenChild(openCards);
+  // openCards.innerHTML = cardPokemon;
+     
+})
+};
+
+cardPokemon(everyPokemon);
+
+//función que permite ordenar según la opción a ordenar
+const differentOrder = document.getElementById("differentOrder");
+
+differentOrder.addEventListener("change", () => {
+  dataContainer.innerHTML = "";
+  const selected = differentOrder.value;
+  const dataOrder = pokemonOrder.differentOrder(everyPokemon, selected);
+  cardPokemon(dataOrder);
+  // console.log("DatAORder", cardPokemon(dataOrder))
+
+})
+
+
+//variable para llamar al selector de clase
+// const theCards = document.querySelector("the-Cards")
+
+//Función para mostrar a los pokemons
+// const showPokemons = (datapoke) => {
+//   let pokemonPack = "";
+
+//   for (const pokemon in data) {
+//     const lookCard = document.createElement("section");
+
+//     //template strings
+//     pokemonPack = `
+//     <section id="lookCard"> 
+//       <section class="frontCard">
+//           <p id=""    
     
-       <figcaption>
-        <p>N°${dataPokemon.num}</p>
-        <p>${dataPokemon.name}</p>
-        <p> CP max: ${dataPokemon.stats["max-cp"]} </p> 
-        <p> HP max: ${dataPokemon.stats["max-hp"]} </p>
-       </figcaption>
-     
-      </figure>`
-     
-});
+//     </section>
+
+
+
+//   }
+
+
+// }
