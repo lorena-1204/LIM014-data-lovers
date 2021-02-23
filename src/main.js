6a86f2b54167bfecdd4d84978b21b5e31082cf1b
lorena-1204 <mlrc1204@gmail.com>
2021-02-23 02:09:00 -0500

    creación de Modal

--- src/main.js
+++ src/main.js
@@ -5,43 +5,87 @@ import datapoke from "./data/pokemon/pokemon.js";
 import { pokemonOrder} from "./data.js";
 
 
+//const containerBorder = document.getElementById(container-border);
+
+
 const dataContainer = document.getElementById("card");
-const btnAz = document.querySelector('.btn-az');
-let btnSort = false;
+
 //variable que llama a toda la data
 let everyPokemon = datapoke.pokemon;
-
--------       ---------  
 
 let cardPokemon = (dataPokemon) => {
   
   dataPokemon.forEach((pokemon) => {
 
-----    ---- ------
+    const container = document.createElement('figure');
 
-    const container = document.createElement('article');
+    container.className= "container-Card "  + pokemon.type[0];
+
     dataContainer.appendChild(container).innerHTML =
      //  utilización de template strings (`)
+     ` <a class="modal-card " href="#modal">
 
-      `<figure class="container-border">
-        <div class="container-Card">
-          <img  src = "${pokemon.img}">
-         
-        
-       
-          <p> N° ${pokemon.num}</p>
-          <p>${pokemon.name}</p>
-          <p> CP max: ${pokemon.stats["max-cp"]} </p> 
-          <p> HP max: ${pokemon.stats["max-hp"]} </p>
-       
-        </div>   
-       </figure>`
+     <img  src = "${pokemon.img}" id="img-card">
+       <figcaption class="legend"> 
+        <p> N° ${pokemon.num}</p>
+        <p>${pokemon.name}</p>
+        <p> CP max: ${pokemon.stats["max-cp"]} </p> 
+        <p> HP max: ${pokemon.stats["max-hp"]} </p>
+       </figcaption> 
+       <nav> 
+         </a>
+       </nav>
+    
+     
+     `
 
-  // dataContainer.appenChild(openCards);
-  // openCards.innerHTML = cardPokemon;
+  // Iconos de type
+  // <p id= "pokemonType"><img class="imgType" alt="" src='img/resistant-weaknesses/${pokemon.type[0]}.png'>
+  //<img class='imgType' alt='' src='img/resistant-weaknesses/${pokemon.type[1]}.png'> 
      
 })
+
+dataPokemon.forEach((pokemon) => {
+
+  const container = document.createElement('figure');
+
+  dataContainer.appendChild(container).innerHTML =
+  ` <section class="modal" id="modal"> 
+      <nav class="modal-content"> 
+       <a class="modal-hide" href="#">✕</a>
+      <figure>
+       <img  src = "${pokemon.img}">
+       <p> N° ${pokemon.num}</p>
+      <p>${pokemon.name}</p>
+      <p>${pokemon.about}</p>
+      <p> CP max: ${pokemon.stats["max-cp"]} </p> 
+      <p> HP max: ${pokemon.stats["max-hp"]} </p>
+
+      <p><b>Resistant: </b></p>
+        <p><img class="imgResistance" alt="" src="img/resistant-weaknesses/${pokemon.resistant[0]}.png">
+        <img class="imgResistance" alt="" src="img/resistant-weaknesses/${pokemon.resistant[1]}.png">
+        <img class="imgResistance" alt="" src="img/resistant-weaknesses/${pokemon.resistant[2]}.png">
+        <img class="imgResistance" alt="" src="img/resistant-weaknesses/${pokemon.resistant[3]}.png">
+        <img class="imgResistance" alt="" src="img/resistant-weaknesses/${pokemon.resistant[4]}.png'>
+        <img class="imgResistance" alt="" src="img/resistant-weaknesses/${pokemon.resistant[5]}.png">
+        <img class="imgResistance" alt="" src="img/resistant-weaknesses/${pokemon.resistant[6]}.png">
+        <img class="imgResistance" alt="" src='img/resistant-weaknesses/${pokemon.resistant[7]}.png'></p>
+
+      <p ><b>Weaknesses:</b></p>
+       <p id="imageContainer"><img class="imgResistance" alt="" src="img/resistant-weaknesses/${pokemon.weaknesses[0]}.png">
+       <img class="imgResistance" alt="" src="img/resistant-weaknesses/${pokemon.weaknesses[1]}.png">
+       <img class="imgResistance" alt="" src="img/resistant-weaknesses/${pokemon.weaknesses[2]}.png">
+       <img class="imgResistance" alt="" src="img/resistant-weaknesses/${pokemon.weaknesses[3]}.png">
+       <img class="imgResistance" alt="" src="img/resistant-weaknesses/${pokemon.weaknesses[4]}.png"></p>
+     
+       </figure>
+      </nav>
+     </section>
+  `
+
+})
 };
+
 
 cardPokemon(everyPokemon);
 
@@ -53,50 +97,29 @@ differentOrder.addEventListener("change", () => {
   const selected = differentOrder.value;
   const dataOrder = pokemonOrder.differentOrder(everyPokemon, selected);
   cardPokemon(dataOrder);
-  
-  // console.log("dataOrder", cardPokemon(dataOrder))
+  // console.log("DatAORder", cardPokemon(dataOrder))
 
 })
 
-//ordenar la data según el tipo seleccionado
-document.getElementById("pokemonType").addEventListener ("change", filterbyType);
 
-//función para filtrar por tipo de pokémons
-function filterbyType() {
-  const filterPokemon = document.getElementById("pokemonType").value;
-  const typesShown = everyPokemon.filter(pokemon =>
-  pokemon.type.includes(filterPokemon));
-  dataContainer.innerHTML = "";
-  cardPokemon(typesShown);
-}
+//variable para llamar al selector de clase
+// const theCards = document.querySelector("the-Cards")
 
-//función para buscar por nombre
-document.querySelector(".search-name").addEventListener("keyup", searchbyName);
+//Función para mostrar a los pokemons
+// const showPokemons = (datapoke) => {
+//   let pokemonPack = "";
 
-function searchbyName() {
-  const searchPokemon = document.getElementById("searchName").value;
-  const pokemonSearched = everyPokemon.filter(pokemon =>
-  pokemon.name.includes(searchPokemon));
-  dataContainer.innerHTML = "";
-  cardPokemon(pokemonSearched); 
-}
+//   for (const pokemon in data) {
+//     const lookCard = document.createElement("section");
 
-//botón alfabético
-// btnAz.addEventListener("click",() => {
-//   if  (btnSort === false) {
-//     dataContainer.innerHTML = "";
-//     btnAz.classList.replace("btn-az","btn-za");
-//     const ascendingAz = pokemonOrder.differentOrder(everyPokemon, orderByName);
-//     cardPokemon(ascendingAz);
+//     //template strings
+//     pokemonPack = `
+//     <section id="lookCard"> 
+//       <section class="frontCard">
+//           <p id=""    
+    
+//     </section>
+
 //   }
-  
-//   if (btnSort === true) {
-//     dataContainer.innerHTML = "";
-//     btnAz.classList.replace("btn-za","btn-az");
-//     const descendingZa = pokemonOrder.differentOrder(everyPokemon,selected);
-//     cardPokemon(descendingZa);
-//   }
-//   btnSort = !btnSort;
 
-// });
-
+// }
